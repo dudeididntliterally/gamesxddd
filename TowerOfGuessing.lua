200a48f25763a3ecf8dbcf4e74f17381a33e4f19
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-08-01 00:51:45 -0400

    Update TowerOfGuessing.lua

--- TowerOfGuessing.lua
+++ TowerOfGuessing.lua
@@ -1,10 +1,10 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
-local Window = Library.CreateLib("Tower Of Badges", "DarkTheme")
+local Window = Library.CreateLib("Tower Of Guessing", "DarkTheme")
 local HomeWindow = Window:NewTab("Home")
 local Home = HomeWindow:NewSection("Home Section")
 
 local workspace = game:GetService("Workspace")
 
-Home:NewButton("Get Badges (30+)", "Home", function()
+Home:NewButton("Get All Badges", "Home", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-479.921051, 4369.55518, 365.876068)
 end)
