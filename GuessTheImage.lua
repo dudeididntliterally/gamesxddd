e430f642921e15c7254798eebf9dc65a98ed2caf
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-08-08 22:17:55 -0400

    Update GuessTheImage.lua

--- GuessTheImage.lua
+++ GuessTheImage.lua
@@ -1,12 +1,20 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local MainWindow = OrionLib:MakeWindow({Name = "Guess The Image", HidePremium = false, SaveConfig = true, ConfigFolder = "GuessTheImage.lua"})
 local MainTab = MainWindow:MakeTab({
-	Name = "Home",
+	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 local MainStuff = MainTab:AddSection({
 	Name = "Main"
+})
+local BackToGamePicker = MainWindow:MakeTab({
+	Name = "Go Back",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local GamePicker = BackToGamePicker:AddSection({
+	Name = "Go Back"
 })
 
 local workspace = game:GetService("Workspace")
@@ -19,4 +27,10 @@ for i,v in pairs(workspace:GetDescendants()) do
         firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
     end
 end
+end})
+
+BackToGamePicker:AddButton({
+Name = "Go Back To Game Picker",
+Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dudeididntliterally/GamePickerMain/main/GamePicker.lua"))()
 end})
